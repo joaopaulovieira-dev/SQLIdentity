ae690da2ed4b3c5142bf3dc9d67d3c6ae0a881ec
João Paulo Vieira <contato@joaopaulovieira.dev>
2022-09-06 15:42:00 -0300

    Login - Parte 01

--- WebApp.Identity/Controllers/HomeController.cs
+++ WebApp.Identity/Controllers/HomeController.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using WebApp.Identity.Models;
 
@@ -28,6 +30,37 @@ namespace WebApp.Identity.Controllers
             return View();
         }
 
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByNameAsync(model.UserName);
+                
+                if (user != null && await _userManager.CheckPasswordAsync(
+                    user, model.Password))
+                {
+                    var identity = new ClaimsIdentity("cookies");
+                    identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, user.Id));
+                    identity.AddClaim(new Claim(ClaimTypes.Name, user.UserName));
+
+                    await HttpContext.SignInAsync("cookie", new ClaimsPrincipal(identity));
+
+                    return RedirectToAction("About");
+                }
+                
+                ModelState.AddModelError("", "Usuário ou senha inválido. ");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
         public async Task<IActionResult> Register(RegisterModel model)
         {
             if (ModelState.IsValid)
--- WebApp.Identity/Models/LoginModel.cs
+++ WebApp.Identity/Models/LoginModel.cs
@@ -2,18 +2,12 @@
 
 namespace WebApp.Identity.Models
 {
-    public class RegisterModel
+    public class LoginModel
     {
         public string UserName { get; set; }
 
         [DataType(DataType.Password)]
 
         public string Password { get; set; }
-
-        [Compare("Password")]
-
-        [DataType(DataType.Password)]
-
-        public string ConfirmPassword { get; set; }
     }
 }
